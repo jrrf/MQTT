cef59e3af32e85e73392be69893a14f095e6d3db
Alcindo Junior <jks_jr@hotmail.com>
2019-03-10 19:20:50 -0300

    Adicionando pasta helpers e atualizando gradle

--- app/src/main/java/com/example/jks_j/mqtt/MainActivity.java
+++ app/src/main/java/com/example/jks_j/mqtt/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_pass;
     private EditText et_topic;
     private static String MQTTHOST = ""; //"tcp://xxxxxxxxxx:porta"
-    private static String USERNAME = "";
-    private static String PASSWORD = "";
+    private static String USERNAME = null;
+    private static String PASSWORD = null;
     private String topicStr = "";
     String clientId = MqttClient.generateClientId();
     MqttAndroidClient client;
@@ -45,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
         bt_ligar = findViewById(R.id.bt_ligar);
         bt_desligar = findViewById(R.id.bt_desligar);
         bt_conect = findViewById(R.id.bt_conect);
-
+/*
         bt_desligar.setEnabled(false);
         bt_ligar.setEnabled(false);
+*/
+        bt_desligar.setVisibility(View.INVISIBLE);
+        bt_ligar.setVisibility(View.INVISIBLE);
 
         bt_conect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,15 +61,20 @@ public class MainActivity extends AppCompatActivity {
                 String input_topic = et_topic.getText().toString();
                 String input_user = et_user.getText().toString();
 
-                if(input_host.isEmpty() || input_porta.isEmpty() || input_pass.isEmpty() || input_topic.isEmpty() || input_user.isEmpty()) {
+                if (input_host.isEmpty() || input_topic.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Preencha todos os campos", Toast.LENGTH_LONG).show();
 
 
-                }else {
+                } else {
+                    if (input_porta.isEmpty())
+                        input_porta = "1883";
                     MQTTHOST = "tcp://" + input_host + ":" + input_porta;
-                    USERNAME = "" + input_user;
                     topicStr = "" + input_topic;
-                    PASSWORD = "" + input_pass;
+
+                    if (!input_user.isEmpty() && !input_pass.isEmpty()) {
+                        USERNAME = input_user;
+                        PASSWORD = input_pass;
+                    }
                     client = new MqttAndroidClient(getApplicationContext(), MQTTHOST, clientId);
                     conectar();
                 }
@@ -89,36 +97,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void conectar(){
-
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setUserName(USERNAME);
-        options.setPassword(PASSWORD.toCharArray());
-
+    public void conectar() {
 
         try {
-            IMqttToken token = client.connect(options);
+            MqttConnectOptions options = new MqttConnectOptions();
+            IMqttToken token;
+            if (USERNAME != null && PASSWORD != null) {
+                options.setUserName(USERNAME);
+                options.setPassword(PASSWORD.toCharArray());
+                token = client.connect(options);
+            } else {
+                token = client.connect();
+            }
+
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    Toast.makeText(getApplicationContext(),"Conectado",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Conectado", Toast.LENGTH_LONG).show();
 
-                    et_host.setEnabled(false);
-                    et_pass.setEnabled(false);
-                    et_topic.setEnabled(false);
-                    et_user.setEnabled(false);
-
-                    bt_conect.setEnabled(false);
-                    bt_desligar.setEnabled(true);
-                    bt_ligar.setEnabled(true);
+                    et_host.setVisibility(View.INVISIBLE);
+                    et_porta.setVisibility(View.INVISIBLE);
+                    et_pass.setVisibility(View.INVISIBLE);
+                    et_topic.setVisibility(View.INVISIBLE);
+                    et_user.setVisibility(View.INVISIBLE);
+                    bt_conect.setVisibility(View.INVISIBLE);
+                    bt_desligar.setVisibility(View.VISIBLE);
+                    bt_ligar.setVisibility(View.VISIBLE);
 
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Toast.makeText(getApplicationContext(),"Não onectado",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Não onectado", Toast.LENGTH_LONG).show();
 
                 }
             });
@@ -128,21 +138,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void ligar(){
+    public void conectarNoUser() {
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(getApplicationContext(), "Conectado", Toast.LENGTH_LONG).show();
+
+                    et_host.setVisibility(View.INVISIBLE);
+                    et_porta.setVisibility(View.INVISIBLE);
+                    et_pass.setVisibility(View.INVISIBLE);
+                    et_topic.setVisibility(View.INVISIBLE);
+                    et_user.setVisibility(View.INVISIBLE);
+                    bt_conect.setVisibility(View.INVISIBLE);
+                    bt_desligar.setVisibility(View.VISIBLE);
+                    bt_ligar.setVisibility(View.VISIBLE);
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(getApplicationContext(), "Não onectado", Toast.LENGTH_LONG).show();
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void ligar() {
         String topic = topicStr;
         String mensagem = "Ligar";
         try {
-            client.publish(topic,mensagem.getBytes(),1,false);
+            client.publish(topic, mensagem.getBytes(), 1, false);
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
 
-    public void desligar(){
+    public void desligar() {
         String topic = topicStr;
         String mensagem = "Desligar";
         try {
-            client.publish(topic,mensagem.getBytes(),1,false);
+            client.publish(topic, mensagem.getBytes(), 1, false);
         } catch (MqttException e) {
             e.printStackTrace();
         }
